bfb8f925fadfc916b335dc961b83911e7321d42c
Barend Venter <barendventer@gmail.com>
2014-05-19 16:49:50 -0700

    Typo fixes and more code commented out. GL context accessors documented

--- src/Graphics/UI/GLFW/Netwire/Token/Core.hs
+++ src/Graphics/UI/GLFW/Netwire/Token/Core.hs
@@ -10,6 +10,7 @@ module Graphics.UI.GLFW.Netwire.Token.Core(
    emptyToken, --Naughty
    getVoidedToken, --OK, but for testing
    GLFWVersion(..),
+   getGLFWVersion,
    Token(..)) where
 
 import qualified Data.Set as Set
@@ -21,10 +22,14 @@ import Control.Monad.Trans.Except
 import System.IO.Unsafe
 import Control.Monad.IO.Class
 import Control.Exception
+import Graphics.UI.GLFW.Netwire.Window.Core
+import Control.Applicative((<$>))
 
 data GLFWError = GLFWError GLFW.Error String deriving (Eq,Ord,Data,Read,Show,Typeable)
 
 type GLFWVersion = GLFW.Version
+
+getGLFWVersion = GLFW.getVersion
 
 instance Exception GLFWError
 
@@ -33,7 +38,7 @@ data GLFWSessionError = GLFWSessionErrorAlreadyInitialized
                       | GLFWSessionErrorAlreadyFinalized
                       | GLFWSessionErrorInitializationFailed
                       | GLFWSessionErrorInvalidToken
-                      | GLFWSessionGLFWErrorOccured GLFWError
+                      | GLFWSessionErrorGLFWErrorOccured GLFWError
                       | GLFWSessionErrorOther String --Should never be used from netwire-glfw-b itself
    deriving(Eq,Show,Ord,Data,Read,Typeable)
 
@@ -56,7 +61,7 @@ checkout :: (MonadIO m) => ExceptT GLFWSessionError m Token
 checkout = do
    alreadyInit <- liftIO $ readIORef glfwIsInitialized
    if alreadyInit then throwE GLFWSessionErrorAlreadyInitialized
-                  else do initializeSuccess <- liftIO $ GLFW.init
+                  else do initializeSuccess <- liftIO GLFW.init
                           if initializeSuccess then liftIO $ do writeIORef glfwIsInitialized True
                                                                 liftIO emptyToken
                                                else throwE GLFWSessionErrorInitializationFailed
@@ -65,6 +70,7 @@ finalizeGLFW :: IO ()
 finalizeGLFW = do
    GLFW.setErrorCallback Nothing
    GLFW.terminate
+
 -- | Finalize GLFW and set the input token to the useless token
 -- | Throws GLFWSessionErrorInvalidToken if argument token is already finalized
 -- | Throws GLFWSessionErrorAlreadyFinalized if GLFW is not running
@@ -85,15 +91,11 @@ voidToken :: Token -> IO ()
 voidToken tk = writeIORef (tokenNaughtyBits tk) (Left ())
 
 getVoidedToken :: IO Token
-getVoidedToken = fmap Token $ newIORef (Left ())
-
---Placeholder
-type Window = ()
+getVoidedToken = Token <$> newIORef (Left ())
 
 data TokenGlobalState = TokenGlobalState {
     errorBuffer  :: IORef [GLFWError],
-    windowBuffer :: IORef [Window],
-    glfwVersion  :: GLFWVersion
+    windowBuffer :: IORef [Window]
     }
 
 emptyWindowBuffer :: [Window]
@@ -118,9 +120,8 @@ emptyToken :: IO Token
 emptyToken = do
    windows <- newIORef emptyWindowBuffer
    errors  <- newIORef emptyErrorBuffer
-   version <- GLFW.getVersion
-   tk <- newIORef (Right (TokenGlobalState errors windows version))
+   tk <- newIORef (Right (TokenGlobalState errors windows))
    GLFW.setErrorCallback $ Just(getErrorHandler errors)
    return $ Token tk
 
-newtype Token = Token { tokenNaughtyBits :: (IORef (Either () TokenGlobalState)) }
+newtype Token = Token { tokenNaughtyBits :: IORef (Either () TokenGlobalState) }
